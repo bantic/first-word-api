321ea3b8758fc38c8b6464fc662d964ee560e33c
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-10 19:06:57 -0400

    add Photo#image_url

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,8 @@ class Photo < ActiveRecord::Base
     copy_to(:image_display) {|i| i.thumb('152x152#') }
   end
   dragonfly_accessor :image_display
+
+  def image_url
+    image_display && image_display.remote_url
+  end
 end
